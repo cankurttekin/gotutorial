663ad9099b22329a1b431793257c282042622ba6
can kurttekin <cankurttekin@gmail.com>
2025-05-18 01:17:58 +0300

    arrays and slices done

--- go-tutorials/cmd/tutorial-4/main.go
+++ go-tutorials/cmd/tutorial-4/main.go
@@ -13,6 +13,7 @@ func main() {
 
 	initArr2 := [2]int32{1, 2}
 	fmt.Println(initArr2)
+	fmt.Println(initArr2[0])
 	
 	var intSlice []int32 = []int32{4,5,6}
 	fmt.Printf("the length of the slice is %v, capacity is %v\n", len(intSlice), cap(intSlice))
@@ -20,4 +21,14 @@ func main() {
 	fmt.Printf("the length of the slice is %v, capacity is %v\n", len(intSlice), cap(intSlice))
 	fmt.Println(intSlice)
 
+	var intSlice2 = []int32{1, 2, 3}
+	intSlice = append(intSlice2, intSlice...)
+	fmt.Println(intSlice)
+
+
+	# you can also specify the length and capacity of a slice
+	var intSlice3 []int32 = make([]int32, 5, 10)
+
+
+	
 }
