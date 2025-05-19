a91f7c493c8411ed8b827082cb98448065b9debf
can kurttekin <cankurttekin@gmail.com>
2025-05-19 21:38:51 +0300

    add channels

--- go-tutorials/cmd/tutorial-9/main.go
+++ go-tutorials/cmd/tutorial-9/main.go
@@ -1,22 +1,53 @@
 package main
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
+var MAX_CHICKEN_PRICE float32 = 5
+var MAX_TOFU_PRICE float32 = 3
+
 func main() {
-	var c = make(chan int, 5) // create a buffered channel with a capacity of 5
-	go process(c) // start a goroutine
-	for i := range c {
-		fmt.Println(i)
-		time.Sleep(1 * time.Second) // simulate some work
+	var chickenChannel = make(chan string)
+	var tofuChannel = make(chan string)
+	var websites = []string{"website1.com", "website2.com", "website3.com"}
+	for i := range websites {
+		go checkChickenPrices(websites[i], chickenChannel)
+		go checkTofuPrices(websites[i], tofuChannel)
+	}
+	sendMessage(chickenChannel, tofuChannel)
+}
+
+func checkTofuPrices(website string, c chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		var tofuPrice = rand.Float32() * 20
+		if tofuPrice<= MAX_TOFU_PRICE {
+			c <- website
+			break
+		}
 	}
 }
 
-func process(c chan int) {
-	defer close(c) // ensure the channel is closed when done
-	for i := 0; i < 5; i++ {
-		c <- i
+func checkChickenPrices(website string, chickenChannel chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		var chickenPrice = rand.Float32() * 20
+		if chickenPrice <= MAX_CHICKEN_PRICE {
+			chickenChannel <- website
+			break
+		}
 	}
-	fmt.Println("exiting process")
 }
+
+func sendMessage(chickenChannel chan string, tofuChannel chan string) {
+	select {
+	case website := <-chickenChannel:
+		fmt.Printf("\nnotification sent: found a deal on chicken at: %s\n", website)
+	case website := <-tofuChannel:
+		fmt.Printf("\nemail sent: found a deal on tofu at: %s\n", website)
+	}
+}
+
+
