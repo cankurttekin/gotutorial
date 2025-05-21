2747643230ae9bb067e8b6175f742d4a35734554
can kurttekin <cankurttekin@gmail.com>
2025-05-21 23:39:32 +0300

    add example 2 values

---
+++ go-by-example/02-values/values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hello" + " go")
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("5.4/2.3 = ", 5.4/2.3)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
