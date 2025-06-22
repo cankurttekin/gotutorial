276b3c4233a71128b57d3f509e1b087ae45f9e87
can kurttekin <cankurttekin@gmail.com>
2025-06-22 20:46:02 +0300

    maps complete

--- go-by-example/10-maps/maps.go
+++ go-by-example/10-maps/maps.go
@@ -1,6 +1,6 @@
 // associative data type
 // like hash or dicts
-// make(map[key-type]val-type)
+// to create empty map > make(map[key-type]val-type)
 // if key not exist, it returns zero value of value type
 // len, delete, clear
 
@@ -12,9 +12,42 @@ import (
 )
 
 func main() {
-	m := map make(map[string]int)
+	m := make(map[string]int)
 
 	m["k1"] = 7
 	m["k2"] = 13
 	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	v3 := m["k3"]
+	fmt.Println("v3:", v3)
+
+	fmt.Println("m", m)
+	fmt.Println("len", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map", m)
+
+	clear(m)
+	fmt.Println("map", m)
+
+	// if key present in map control
+	_, prs := m["k2"]
+	fmt.Println("prs", prs)
+
+	n := map[string]int{"foo": 1, "bar" : 2}
+	fmt.Println("map", n)
+
+	n2 := map[string]int{"foo": 1, "bar" : 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n == n2")
+	}
+
+}
+
+
+
+
 
